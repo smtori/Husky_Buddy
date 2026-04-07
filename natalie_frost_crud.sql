6d1fd2fa0364f7d7bba07d0470a3d7dcdad168df
Aarish Kodnaney <aarishkod@gmail.com>
2026-04-06 22:27:26 -0400

    3/4

---
+++ natalie_frost_crud.sql
@@ -0,0 +1,51 @@
+USE `husky-buddy-orig`;
+
+-- natalie frost #1 (3.1)
+UPDATE student_major_tags
+SET major_id = (
+    SELECT major_id FROM majors
+    WHERE major_name = 'Computer Science'
+)
+WHERE student_id = 123456;
+
+-- natalie frost #2 (3.2)
+DELETE FROM student_interest
+WHERE interest_id = (
+    SELECT tag_id FROM interest_tag
+    WHERE tag_name = 'Tennis'
+);
+
+-- natalie frost #3 (3.3)
+ALTER TABLE husky_user
+ADD CONSTRAINT check_northeastern_email
+CHECK (nu_email LIKE '%@northeastern.edu');
+
+
+-- natalie frost #4 (3.4)
+SELECT firstName, lastName from husky_user
+INNER JOIN husky_match ON husky_match.student2_id = husky_user.student_id
+WHERE husky_user.student_id = 1; -- should be logged in user
+
+
+-- natalie frost #5 (3.5)
+INSERT INTO flag_report (reporter_id, reported_id, reason, status, created_at)
+VALUES (1, 2, 'uncomfortable interaction during coffee chat', 'under review', NOW());
+DELETE FROM husky_match
+WHERE (student1_id = 1 AND student2_id = 2)
+OR (student2_id = 1 AND student1_id =2 );
+
+
+-- natalie frost #6 (3.6)
+INSERT INTO student_interest (student_id, interest_id)
+VALUES
+    (123456, 1),
+    (123456, 2),
+    (123456, 3),
+    (123456, 4);
+
+INSERT INTO interest_tag (tag_name)
+VALUES
+    ('football'),
+    ('golf'),
+    ('running'),
+    ('fortnite');
