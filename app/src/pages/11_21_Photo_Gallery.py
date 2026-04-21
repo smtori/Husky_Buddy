be26b946f541c08b4b35e3db120bad5c5111c10a
Tori Smith <smithmtori@gmail.com>
2026-04-21 11:23:04 -0400

    change navigation

--- app/src/pages/11_21_Photo_Gallery.py
+++ app/src/pages/11_21_Photo_Gallery.py
@@ -27,7 +27,7 @@ if not return_page:
         return_page = 'pages/10_Brandon_Home.py'
 
 if st.button("← Back to Options", type="secondary", use_container_width=False):
-    st.switch_page('pages/10_Brandon_Home.py')
+    st.switch_page(return_page)
 st.header(f"My HuskyBuddy Photo Gallery")
 st.write(f"### Hi, {st.session_state['first_name']}.")
 
